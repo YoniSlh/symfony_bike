fe5c5df2da661a2a2ae8baf586cb207e955922b4
Yoni Slh <yonisel12@gmail.com>
2024-11-13 11:34:34 +0100

    edit et ajout produit OK

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use App\Repository\OrderRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\ImageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,18 +14,26 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
 use App\Entity\Product;
+use App\Entity\Category;
+use App\Entity\Image;
+use Doctrine\Common\Collections\ArrayCollection;
+use App\Entity\ProductStatus;
 
 class AdminController extends AbstractController
 {
     private UserRepository $userRepository;
     private ProductRepository $productRepository;
     private OrderRepository $orderRepository;
+    private CategoryRepository $categoryRepository;
+    private ImageRepository $imageRepository;
 
-    public function __construct(UserRepository $userRepository, ProductRepository $productRepository, OrderRepository $orderRepository)
+    public function __construct(UserRepository $userRepository, ProductRepository $productRepository, OrderRepository $orderRepository, CategoryRepository $categoryRepository, ImageRepository $imageRepository)
     {
         $this->userRepository = $userRepository;
         $this->productRepository = $productRepository;
         $this->orderRepository = $orderRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->imageRepository = $imageRepository;
     }
 
     #[Route('/admin', name: 'app_admin')]
@@ -50,6 +60,9 @@ class AdminController extends AbstractController
     #[Route('/admin/edit/product/{id}', name: 'app_admin_editProduct')]
     public function editProduct(Product $product, EntityManagerInterface $entityManager, Request $request): Response
     {
+        $categories = $this->categoryRepository->findAll();
+        $productStatus = ProductStatus::cases();
+
         if ($request->isMethod('POST')) {
             $product->setName($request->request->get('name'));
             $product->setPrice($request->request->get('price'));
@@ -59,13 +72,80 @@ class AdminController extends AbstractController
             $product->setCouleur($request->request->get('couleur'));
             $product->setMarque($request->request->get('marque'));
 
+            // $imageUrls = $request->request->get('images');
+            // if (!is_array($imageUrls)) {
+            //     $imageUrls = [$imageUrls];
+            // }
+
+            // $images = [];
+            // foreach ($imageUrls as $imageUrl) {
+            //     $image = new Image();
+            //     $image->setUrls([$imageUrl]);
+            //     $images[] = $image;
+            // }
+            // foreach ($images as $image) {
+            //     $product->addImage($image);
+            // }
+
+            $status = $request->request->get('status');
+            if (ProductStatus::tryFrom($status)) {
+                $product->setStatus(ProductStatus::from($status));
+            }
+
+            $categoryId = $request->request->get('category');
+            $category = $this->categoryRepository->find($categoryId);
+            if ($category) {
+                $product->setCategory($category);
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_admin');
         }
 
         return $this->render('editProduct.html.twig', [
-            'product' => $product
+            'product' => $product,
+            'categories' => $categories,
+            'productStatus' => $productStatus
+        ]);
+    }
+    #[Route('/admin/addProduct', name: 'app_admin_addProduct')]
+    public function addProduct(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $categories = $this->categoryRepository->findAll();
+        $productStatus = ProductStatus::cases();
+        $product = new Product();
+
+        if ($request->isMethod('POST')) {
+            $product->setName($request->request->get('name'));
+            $product->setPrice($request->request->get('price'));
+            $product->setDescription($request->request->get('description'));
+            $product->setStock($request->request->get('stock'));
+            $product->setPoids($request->request->get('poids'));
+            $product->setCouleur($request->request->get('couleur'));
+            $product->setMarque($request->request->get('marque'));
+
+            $categoryId = $request->request->get('category');
+            $category = $this->categoryRepository->find($categoryId);
+            if ($category) {
+                $product->setCategory($category);
+            }
+
+            $status = $request->request->get('status');
+            if (ProductStatus::tryFrom($status)) {
+                $product->setStatus(ProductStatus::from($status));
+            }
+
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin');
+        }
+
+        return $this->render('addProduct.html.twig', [
+            'categories' => $categories,
+            'product' => $product,
+            'productStatus' => $productStatus
         ]);
     }
 }
