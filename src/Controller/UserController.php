b49b5f018c6264ce7073650a553c6908296654dd
Yoni Slh <yonisel12@gmail.com>
2024-11-26 01:39:38 +0100

    gestion complète des CB ok

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use App\Entity\CreditCard;
 
 class UserController extends AbstractController
@@ -30,20 +30,16 @@ class UserController extends AbstractController
     #[Route('/profil', name: 'user_profil')]
     public function listCreditCards(EntityManagerInterface $entityManager): Response
     {
-        $user = $this->getUser(); 
-
-        $creditCards = $entityManager->getRepository(CreditCard::class)->findBy([
-            'user' => $user
-        ]);
+        $user = $this->getUser();
+        $creditCards = $entityManager->getRepository(CreditCard::class)->findBy(['user' => $user]);
 
         return $this->render('profil.html.twig', [
-            'controller_name' => 'UserController',
             'creditCards' => $creditCards,
         ]);
     }
 
     #[Route('/profil/addCreditCard', name: 'user_profil_addCreditCard')]
-    public function addCreditCard(EntityManagerInterface $entityManager, Request $request): Response
+    public function addCreditCard(Request $request, EntityManagerInterface $entityManager): Response
     {
         if ($request->isMethod('POST')) {
             $card = new CreditCard();
@@ -55,10 +51,47 @@ class UserController extends AbstractController
             $entityManager->persist($card);
             $entityManager->flush();
 
-            $this->addFlash('success', 'La carte bancaire a bien été ajoutée');
+            $this->addFlash('success', 'La carte bancaire a bien été ajoutée.');
             return $this->redirectToRoute('user_profil');
         }
 
         return $this->render('addCreditCard.html.twig');
+    }
+
+    #[Route('/profil/editCreditCard/{id}', name: 'user_profil_editCreditCard')]
+    public function editCreditCard(int $id, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $card = $entityManager->getRepository(CreditCard::class)->find($id);
+        if (!$card || $card->getUser() !== $this->getUser()) {
+            return $this->redirectToRoute('user_profil');
+        }
+
+        if ($request->isMethod('POST')) {
+            $card->setNumber($request->request->get('card_number'));
+            $card->setExpirationDate(new \DateTime($request->request->get('expiration_date')));
+            $card->setCvv($request->request->get('cvv'));
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La carte bancaire a bien été modifiée.');
+            return $this->redirectToRoute('user_profil');
+        }
+
+        return $this->render('editCreditCard.html.twig', [
+            'card' => $card,
+        ]);
+    }
+
+    #[Route('/profil/deleteCreditCard/{id}', name: 'user_profil_deleteCreditCard')]
+    public function deleteCreditCard(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $card = $entityManager->getRepository(CreditCard::class)->find($id);
+        if ($card && $card->getUser() === $this->getUser()) {
+            $entityManager->remove($card);
+            $entityManager->flush();
+        }
+
+        $this->addFlash('success', 'La carte bancaire a bien été supprimée.');
+        return $this->redirectToRoute('user_profil');
     }
 }
